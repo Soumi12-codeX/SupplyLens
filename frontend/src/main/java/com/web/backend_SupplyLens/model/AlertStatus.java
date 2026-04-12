b8db51221da4a8ebe42ecff6c45b9404599aa90c
Arka Roy <royarka309@gmail.com>
2026-04-12 22:05:00 +0530

    Remove redundant java backend files from frontend src folder

--- frontend/src/main/java/com/web/backend_SupplyLens/model/AlertStatus.java
+++
@@ -1,5 +0,0 @@
-package com.web.backend_SupplyLens.model;
-
-public enum AlertStatus {
-    PENDING, ACCEPTED, DISMISSED
-}
--- frontend/src/main/java/com/web/backend_SupplyLens/model/RouteOptionStatus.java
+++
@@ -1,5 +0,0 @@
-package com.web.backend_SupplyLens.model;
-
-public enum RouteOptionStatus {
-    PENDING, SELECTED, REJECTED
-}
