b5f8df3b7072ef36354ada6a4634e9804d8aa4ca
Soumi12-codeX <dsoumi192@gmail.com>
2026-04-27 18:15:41 +0530

    modified api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : 'http://localhost:8080/api',
+    baseURL : ' https://supplylens-4n7e.onrender.com/api',
     headers : {
         'Content-Type' : 'application/json'
     }
